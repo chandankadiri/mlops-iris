54aa7a83b6947fccd592266dbc8906a82e4edc17
chandankadiri <chandankadiri82@gmail.com>
2021-07-06 19:11:49 +0530

    changed dones

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from ml_utils import load_model, predict, retrain
 from typing import List
+from datetime import datetime
 
 # defining the main app
 app = FastAPI(title="Iris Predictor", docs_url="/")
@@ -22,6 +23,7 @@ class QueryIn(BaseModel):
 # class which is returned in the response
 class QueryOut(BaseModel):
     flower_class: str
+    timestamp:str
 
 # class which is expected in the payload while re-training
 class FeedbackIn(BaseModel):
@@ -43,7 +45,9 @@ def ping():
 # Payload: QueryIn containing the parameters
 # Response: QueryOut containing the flower_class predicted (200)
 def predict_flower(query_data: QueryIn):
-    output = {"flower_class": predict(query_data)}
+    now = datetime.now()
+    output = {"flower_class": predict(query_data),'timestamp': now.strftime("%m/%d/%Y %H:%M:%S")}
+    print(output)
     return output
 
 @app.post("/feedback_loop", status_code=200)
@@ -58,4 +62,5 @@ def feedback_loop(data: List[FeedbackIn]):
 # Main function to start the app when main.py is called
 if __name__ == "__main__":
     # Uvicorn is used to run the server and listen for incoming API requests on 0.0.0.0:8888
-    uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
+    #uvicorn.run("main:app", host="0.0.0.0", port=8888, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=5000, reload=True) #changed the host and port
--- ml_utils.py
+++ ml_utils.py
@@ -1,10 +1,22 @@
+from math import pi
 from sklearn import datasets
+from sklearn import neighbors
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+import numpy as np
+import os
+import pickle
 
 # define a Gaussain NB classifier
-clf = GaussianNB()
+#clf = GaussianNB()
+classifiers=[GaussianNB(),RandomForestClassifier(n_estimators=200),KNeighborsClassifier(n_neighbors=6,n_jobs=-1),LogisticRegression(max_iter=1000)]
+names=['gaussianNB','randomforestclassifier','KNNClassifier','LogisticRegression']
+
+
 
 # define the class encodings and reverse encodings
 classes = {0: "Iris Setosa", 1: "Iris Versicolour", 2: "Iris Virginica"}
@@ -17,15 +29,30 @@ def load_model():
 
     # do the test-train split and train the model
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-    clf.fit(X_train, y_train)
+    model_list=[]
+    accuracy=[]
+    for clf in classifiers:
+        #clf.fit(X_train, y_train)
+        clf.fit(X_train,y_train)
 
-    # calculate the print the accuracy score
-    acc = accuracy_score(y_test, clf.predict(X_test))
-    print(f"Model trained with accuracy: {round(acc, 3)}")
+        # calculate the print the accuracy score
+        acc = accuracy_score(y_test, clf.predict(X_test))
+        print(f"Model {clf} trained with accuracy: {round(acc, 3)}")
+        accuracy.append(acc)
+        model_list.append(clf)
+        acc=0
+
+    
+    acc_index=np.argmax(accuracy)
+
+    loaded_model=model_list[acc_index]
+    print(f"loading {loaded_model} with accuracy {accuracy[acc_index]}")
+    pickle.dump(loaded_model,open('model.pkl','wb'))
 
 
 # function to predict the flower using the model
 def predict(query_data):
+    clf=pickle.load(open('model.pkl','rb'))
     x = list(query_data.dict().values())
     prediction = clf.predict([x])[0]
     print(f"Model prediction: {classes[prediction]}")
@@ -34,6 +61,7 @@ def predict(query_data):
 # function to retrain the model as part of the feedback loop
 def retrain(data):
     # pull out the relevant X and y from the FeedbackIn object
+    clf=pickle.load(open('model.pkl','rb'))
     X = [list(d.dict().values())[:-1] for d in data]
     y = [r_classes[d.flower_class] for d in data]
 
